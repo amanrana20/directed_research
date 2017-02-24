be34b93d76456d3458a3186020b3b3c9f92dd667
Aman Rana <amanrana20@gmail.com>
2017-02-24 16:08:07 -0500

    updating

--- main.py
+++ main.py
@@ -1,118 +1,114 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import os, cv2
-import dicom
-import scipy.ndimage
-import CNN
+'''
+Author: Aman Rana
+email: arana@wpi.edu
+
+About: This sile contains code for overall code management and execution
+'''
 
 
-# Setting the global parameters
-PATH_BASE = 'data/'
-print('\n{} {}'.format('Files in data folder:', os.listdir(PATH_BASE)))
-EXT_DATASET = 'stage1'
-EXT_STAGE1_LABELS = 'stage1_labels.csv'
-EXT_SAMPLE_SUBMISSION = 'stage1_sample_submission.csv'
-patients_names = os.listdir(os.path.join(PATH_BASE, EXT_DATASET))
-print('Number of Patients: {}'.format(len(patients_names)))
+import numpy as np
+import cv2, os
+import SimpleITK as itk
+import tensorflow as tf
+
+import sys
+
+sys.path.insert(0, 'Modified_U_Net')
+from model import *
 
 
-# VARIABLES
-generator_batch_size = 1
-TRAINING_BATCH_SIZE = 1
-CONSTANT_3D_IMAGE_SHAPE = (512, 512, 512)
-NUMBER_OF_TRAINING_ITERATIONS = 1
-
-
-# Reading the ground truth labels and getting the ratio of both classes
-csv_ground_truth = pd.read_csv(os.path.join(PATH_BASE, EXT_STAGE1_LABELS))
-cancer = len(csv_ground_truth[csv_ground_truth['cancer'] == 1])
-no_cancer = len(csv_ground_truth[csv_ground_truth['cancer'] == 0])
-print '# cancer cases: {0}\n# non cancre cases: {1}\n'.format(cancer, no_cancer)
-
-
-print 'NOTE: The number of cancer patients is {0} times more than non-cancer patients. We will have to modify the loss function to accomodate the imblance in training data by giving more importance to cancer cases by a factor of {0}.\n'.format(no_cancer*1./cancer)
-
-
-# This function generates the batches. Each batch contains <batch_size> patients with their respective scans. All scans are ordered.
-def generate_patient_batches(batch_size):
-	for count in range(0, generator_batch_size, batch_size):
-		patients = {}
-		for i, patients_name in enumerate(patients_names[count: count+batch_size]):
-		    label = 1 if csv_ground_truth.loc[csv_ground_truth['id'] == patients_name]['cancer'].any() else 0
-		    patients[patients_name] = {'label': label}
-		yield patients
-
-
-def create_patient_data(patient, label):
-	path_patient= os.path.join(PATH_BASE, EXT_DATASET, patient)
-	patient_slices = os.listdir(path_patient)
-
-	dcms = []
-	for slice in patient_slices:
-		dcm = dicom.read_file(os.path.join(path_patient, slice))
-		dcms.append(dcm)
-	dcms.sort(key = lambda z: int(z.InstanceNumber))
-	image_3d = [np.array(each_dcm.pixel_array).astype(np.float16) for each_dcm in dcms]
-
-	pixel_spacing = dcms[0].PixelSpacing
-	slice_thickness = abs(dcms[0].SliceLocation - dcms[1].SliceLocation)
-	rescal_intercept = int(dcms[0].RescaleIntercept)
-	rescale_slope = int(dcms[0].RescaleSlope)
-
-	return {'3d image': image_3d, 'label': label, 'pixel spacing': pixel_spacing, 'slice thickness': slice_thickness, 'slope': rescale_slope, 'intercept': rescal_intercept}
-
-
-def process_3d_images(data):
-	# Preprocessing
-	img = np.array(data['3d image'])
-	img[img == -2000.] = 0
-	img = (img * data['slope']) + data['intercept']
-
-	# 3D image interpolation
-	img = scipy.ndimage.interpolation.zoom(img, [data['slice thickness'], 1., 1.], mode='nearest')
-
-	# Making all 3D images of a constant size
-	num_slices = img.shape[0]
-	img_const = np.zeros(shape=CONSTANT_3D_IMAGE_SHAPE)
-	img_const += img[0, 0, 0]
-	for i in range(num_slices):
-		pos = i + int((CONSTANT_3D_IMAGE_SHAPE[0] - num_slices) / 2)
-		img_const[pos, :, :] = img[i, :, :]
-
-	return np.array(img_const).astype(np.float16)
-
-
-batch_generator = generate_patient_batches(generator_batch_size)
-
-
-def training_batch():
-	batch = []
-	each_batch = batch_generator.next()
-
-	for patient in each_batch.keys():
-		patient_data = create_patient_data(patient, each_batch[patient]['label'])
-		
-		# Making all the images of constant size
-		patient_data['3d image'] = np.array(process_3d_images(patient_data)).reshape(1, 512, 512, 512)
-
-		# Making training batch
-		batch.append([patient_data['3d image'].astype(np.float16), patient_data['label']])	
-	batch = np.array(batch)
-	
-	return batch
-
-
-def train_model():
-	model = CNN.model((1, 512, 512, 512), 1)
-	# print model.summary()
-	for training_iteration in range(NUMBER_OF_TRAINING_ITERATIONS):
-		batch = training_batch()
-		X = np.array([x for x in batch[:, 0]]).reshape(batch.shape[0], 1, 512, 512, 512)
-		X = X.astype(np.float16)
-		Y = np.array([int(y) for y in batch[:, 1]]).astype(np.float16)
-
-		model.fit(X, Y, batch_size=TRAINING_BATCH_SIZE, nb_epoch=TRAINING_BATCH_SIZE, verbose=1)
+# Costants
+PATH_TRAINING_DATASET = '../../kaggle_main/Data Science Bowl Kaggle/dataset/Annotated Lung Cancer Dataset/training data'
+EPOCHS = 5
+GENERATOR_BATCH_SIZE = 8
 
 
 
-train_model()
+training_files = os.listdir(PATH_TRAINING_DATASET)
+training_files.sort()
+l = len(training_files)
+
+
+## generating batch
+def get_batch():
+    
+	for i in range(0, l, GENERATOR_BATCH_SIZE*2):
+
+		batch_files = training_files[i: i+GENERATOR_BATCH_SIZE*2]
+
+		l1 = len(batch_files)
+
+		X = np.zeros([l1/2, 512, 512, 1])
+		Y = np.zeros([l1/2, 512, 512, 1])
+
+		for j in range(0, l1, 2):
+
+		    x = batch_files[j]
+		    y = batch_files[j+1]
+
+		    path_to_x = os.path.join(PATH_TRAINING_DATASET, x)
+		    path_to_y = os.path.join(PATH_TRAINING_DATASET, y)
+
+		    img = np.array(np.load(path_to_x))[1, :, :]
+		    mask = np.array(np.load(path_to_y))[1, :, :]
+
+		    X[j/2, :, :, :] = img.reshape([512, 512, 1])
+		    Y[j/2, :, :, :] = mask.reshape([512, 512, 1])
+
+		yield {'X': X, 'Y': Y}
+
+
+## train
+def train():
+
+	with tf.name_scope('Placeholders'):
+
+		x = tf.placeholder(tf.float32, shape=[None, 512, 512, 1], name='x')
+		y = tf.placeholder(tf.float32, shape=[None, 512, 512, 1], name='y')
+		k = tf.placeholder(tf.float32, name='k')
+
+		init = tf.global_variables_initializer()
+
+	prediction = pred(x)
+
+	with tf.name_scope('Loss'):
+
+		loss = dice_coef_loss(y, prediction, k)
+		tf.summary.scalar('loss', loss)
+
+
+	with tf.name_scope('Optimizer'):
+
+		optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+
+	with tf.Session() as sess:
+
+		sess.run(init)
+
+		counter = 0
+
+		for batch in get_batch():
+
+			counter += 1
+
+			batch_X = np.array(batch['X']).astype('float')
+			batch_Y = np.array(batch['Y']).astype('float')
+
+			# print batch_X.shape, batch_Y.shape
+
+			l, _ = sess.run([loss, optimizer], feed_dict={x: batch_X, y: batch_Y, k: batch_X.shape[0]})
+			
+			print 'Iteration: {0}\tLoss: {1}'.format(counter, l)
+
+			if counter >= 130:
+
+				break
+
+		# correct_pred = dice_coef(pred(x), y, batch_X.shape[0])
+		# accuracy = tf.
+		# merged = tf.summary.merge_all()
+		# tf.summary.FileWriter('.', sess.graph)
+
+
+train()
